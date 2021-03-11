ec6c32b5e7f0694d396a5f042396f7a89be62123
Mohsin <mohsinafzal29@gmail.com>
2021-03-11 08:34:22 +0100

    Set up vue and recomended configs for frontend and implement bacis foodangel component

---
+++ frontend/src/components/angel-card.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name:'AngelCard',
+    props:['user']
+}
+</script>
+
+<template lang="pug">
+.box
+    h2 {{user.name}}
+    h3 AvailableMeal(s):({{user.availableMeal}})
+</template>
+
+<style lang="scss" scoped>
+ .box{
+     padding:2rem;
+     border:1px solid #333;
+     background:#dd0;
+     border-radius:0.3rem;
+ }
+</style>
