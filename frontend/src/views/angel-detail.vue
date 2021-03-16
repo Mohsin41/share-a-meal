9beacbc75ae47d92be578aa0838a3b1ce39ddcce
Mohsin <mohsinafzal29@gmail.com>
2021-03-16 14:55:19 +0100

    Set up angel-detail view component

---
+++ frontend/src/views/angel-detail.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios'
+import AngelCard from '@/components/angel-card.vue'
+export default {
+  name: 'AngelDetail',
+  components: {
+    AngelCard,
+  },
+  data() {
+    return {
+      user: {},
+    }
+  },
+  async created() {
+    const userRequest = await axios.get(`/api/users/${this.$route.params.id}`)
+    this.user = userRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .about
+    <h1>Here is the detail view</h1>
+    AngelCard(:user="user" v-if="user")
+</template>
