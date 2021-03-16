19d6ef7fbe0a005324d3eb6f9b0f8846afd5d88d
Mohsin <mohsinafzal29@gmail.com>
2021-03-16 14:55:01 +0100

    Set up angel-list view component

---
+++ frontend/src/views/angel-list.vue
@@ -0,0 +1,28 @@
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import AngelCard from '@/components/angel-card.vue'
+export default {
+  name: 'FoodAngels',
+  components: {
+    AngelCard,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+    this.users = usersRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Share a Meal
+    h2 Angels
+    div(v-for="user in users" )
+      router-link(:to="`/users/${user._id}`") {{user.name}}
+</template>
