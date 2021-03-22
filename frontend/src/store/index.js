00dce3aa635d53f7dda16f485691fc5bdca9ca97
Mohsin <mohsinafzal29@gmail.com>
2021-03-22 18:19:14 +0100

    Implement login and register actions  in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,9 +4,18 @@ import axios from 'axios'
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+const mutations = {
+  SET_USER: 'set user'
+}
+
+const store = new Vuex.Store({
+  state: {user: null},
+  mutations: {
+    [mutations.SET_USER](state, user)
+  {
+      state.user = user
+    }
+    },
   actions: {async fetchUser(store, id) {
       const userRequest = await axios.get(`/api/users/${id}`)
       return userRequest.data
@@ -14,7 +23,31 @@ export default new Vuex.Store({
     async fetchUsers() {
       const usersRequest = await axios.get('/api/users')
       return usersRequest.data
+    },
+     async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session')
+      commit(mutations.SET_USER, user.data || null)
+    },
+    async login({ commit }, credentials) {
+      try {
+        const user = await axios.post('/api/account/session', credentials)
+        commit(mutations.SET_USER, user.data)
+      } catch (e) {
+        throw e
+      }
+    },
+    async register(store, user) {
+      return axios.post('/api/account', user)
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session')
+      commit(mutations.SET_USER, null)
     }
+
 },
   modules: {}
 });
+export default async function init() {
+  await store.dispatch('fetchSession')
+  return store
+}
